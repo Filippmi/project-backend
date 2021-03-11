987779ba2a0a19040b291e92291ba21c5031222e
Filipp Minayev <Filippmi@gmail.com>
2021-03-11 07:38:10 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create([
+  {name: "Basement remodel", description: "Adding home theater"},
+  {name: "Kitchen remodel", description: "Start date: 03/25/21 Finish by: 04/01/22"}
+])
+
+Todo.create([
+  {task: "Calculate budget"},
+  {task: "Find designer"},
+  {task: "Find contractor"},
+  {task: "Figure out what the heck is going on lol"}
+])
