cdcb020879816bffabefd5c51dd5cb7aa5855bb2
Filipp Minayev <Filippmi@gmail.com>
2021-03-10 23:24:47 -0700

    migrated todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_060303) do
+ActiveRecord::Schema.define(version: 2021_03_11_062051) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "task"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
